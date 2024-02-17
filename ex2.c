e859534124434168507a2239b2dce598a3c4b166
Arthur-Nevess <arthurneves0a12@gmail.com>
2024-02-16 22:06:24 -0300

    corigir erro

--- ex2.c
+++ ex2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main()
 {
@@ -10,22 +11,88 @@ int main()
     int enforcou=0;
     int tentativa=0;
     char palpite;
-
+    int usuario=0;
+    
+    
 
     puts("==============");
     puts("JOGO DE FORCA");
     puts("==============");
 
+    puts("Qual tipo de palavra você quer tentar acertar?");
+    puts("Para frutas, tecle 1");
+    puts("Para estados do Brasil, tecle 2");
+    puts("Para profissões, tecle 3");
+    scanf("%d", &usuario);
+    FILE *p;
+    p = fopen ("exa.txt", "r+");
 
-    sprintf(palavras, "Jurema");
+
+
+   switch(usuario)
+   {
+        case 1:
+        {         
+            if(p == NULL)
+            {
+                puts("Erro ao tentar executar o arquivo");
+                return 0;
+            }
+
+            
+            
+            int qtddepalavas;
+            fscanf(p,"%d", &qtddepalavas);
+            srand(time(0));
+            int conta = rand() % qtddepalavas;
+
+            for(int i = 0; i <= conta;i++)
+            {
+                fscanf(p,"%s", palavras);
+            } 
+            
+
+          fclose(p);
+        }  
+
+        case 2:
+        {
+            FILE *p = fopen ("exb.txt", "r");
+                    
+            if(p == NULL)
+            {
+                puts("Erro ao tentar executar o arquivo");
+            }
+
+            else
+            {
+                int qtddepalavas;
+                fscanf(p,"%d", &qtddepalavas);
+                srand(time(0));
+                int conta = rand() % qtddepalavas;
+
+                for(int i = 0; i <= conta;i++)
+                {
+                    fscanf(p,"%s", palavras);
+                } 
+            }
+
+                fclose(p);
+        }  
+
+
+
+   }
     
+    
+
     do
     {
-        for(int i=0; i<(sizeof(palavras))/4;i++)
+        for(int i = 0; i < strlen(palavras); i++)
         {
 
             int acertou=0;
-            for(int j=0; j<tentativa;j++)
+            for(int j = 0; j < tentativa;j++)
             {
                 if(palavras[i]==chute[j])
                 {
@@ -33,32 +100,29 @@ int main()
                     break;
                 }
             }
+
             if(acertou)
             {
-                printf("%c", palavras[i]);
-                break;
+                printf("%c ", palavras[i]);
+                
             }
             
             else
             {
                 printf("_ ");
-                break;
+                
             }
+        
         }
-
             printf("\n");
 
+            
             printf("DIGITE AQUI:");
-            scanf(" %c", palpite);
+            scanf(" %c", &palpite);
             chute[tentativa]=palpite;
             tentativa++; 
-                
+              
     } while (!acertou && !enforcou);
 
-    
-
-    
-
-    
     return 0;
 }
